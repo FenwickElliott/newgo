90ba093fe72457cca5fc7ee3f4376515eb42fd08
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-02-08 17:27:19 -0500

    taking name from flag and checking err

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "os"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var name = flag.String("name", "", "Name of project to be created.")
 
 func main() {
-	os.Mkdir("name", 0755)
+	flag.Parse()
+	err := os.Mkdir(*name, 0755)
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
 }
