45414f8c2a67b576fe9deac1471f5da3633796f8
Charles Fenwick Elliott <charles@fenwickelliott.io>
2019-04-02 09:22:56 -0400

    initialize

--- main.go
+++ main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"path"
 )
 
-var name = flag.String("name", "", "Name of project to be created.")
+func main() {
+	fatal(ioutil.WriteFile("main.go", []byte(`package main
+
+import (
+	"log"
+
+	"github.com/brideclick/initialize"
+)
 
 func main() {
-	flag.Parse()
-	err := os.Mkdir(*name, 0755)
-	check(err)
-	err = ioutil.WriteFile(path.Join(".", *name, "main.go"), []byte("package main\n\nfunc main() {\n\n}\n"), 0644)
+
 }
 
-func check(err error) {
+func init() {
+	err := initialize.Viper()
+	fatal(err)
+}
+
+func fatal(err error) {
 	if err != nil {
-		fmt.Println("Error: ", err)
+		log.Fatal(err)
+	}
+}
+
+func handle(err error) bool {
+	if err != nil {
+		log.Println(err)
+		return true
+	}
+	return false
+}
+
+func print(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}`), 0644))
+
+	fatal(os.Mkdir("config", 0755))
+
+	fatal(ioutil.WriteFile(path.Join("config", "dev.json"), []byte("{\n\n}"), 0644))
+	fatal(ioutil.WriteFile(path.Join("config", "prod.json"), []byte("{\n\n}"), 0644))
+}
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatal(err)
 	}
 }
